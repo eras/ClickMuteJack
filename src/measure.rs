ce2ca8d2232444eea69532764e8bd537664e3fb5
Erkki Seppälä <erkki.seppala@vincit.fi>
2021-09-12 15:12:25 +0300

    measure: support explicit sampling, not just measuring function call times

--- src/measure.rs
+++ src/measure.rs
@@ -11,7 +11,7 @@ where
 }
 
 pub struct Repeated {
-    num_calls: u32,
+    num_samples: u32,
     total_time: Duration,
     prev_time: Duration,
 }
@@ -19,7 +19,7 @@ pub struct Repeated {
 impl Repeated {
     pub fn new() -> Self {
         Repeated {
-            num_calls: 0,
+            num_samples: 0,
             total_time: std::default::Default::default(),
             prev_time: std::default::Default::default(),
         }
@@ -30,17 +30,37 @@ impl Repeated {
         F: FnOnce() -> T,
     {
         let (time, retval) = measure(f);
-        self.num_calls += 1;
+        self.num_samples += 1;
         self.total_time += time;
         self.prev_time = time;
         retval
     }
 
+    pub fn sample(&mut self, mark: &Mark) {
+        let now = Instant::now();
+        let time = now - mark.time;
+        self.num_samples += 1;
+        self.total_time += time;
+        self.prev_time = time;
+    }
+
     pub fn average(&self) -> Duration {
-        self.total_time / self.num_calls
+        self.total_time / self.num_samples
     }
 
     pub fn prev_time(&self) -> Duration {
         self.prev_time
+    }
+}
+
+pub struct Mark {
+    time: Instant,
+}
+
+impl Mark {
+    pub fn new() -> Self {
+        Mark {
+            time: Instant::now(),
+        }
     }
 }
